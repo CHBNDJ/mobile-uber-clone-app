1e556322fbfcd320ebe75d9294c19107ce7fea47
Chabane Nadji <chabane.nadji@gmail.com>
2025-08-07 10:36:07 +0200

    update of the sign in page

--- app/(root)/(tabs)/home.tsx
+++ app/(root)/(tabs)/home.tsx
@@ -6,13 +6,7 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 export default function Home() {
   const { user } = useUser();
 
-  return (
-    <SafeAreaView>
-      <SignedIn>
-        <Text>Hello {user?.emailAddresses[0].emailAddress}</Text>
-      </SignedIn>
-    </SafeAreaView>
-  );
+  return <SafeAreaView></SafeAreaView>;
 }
 
 const psql =
